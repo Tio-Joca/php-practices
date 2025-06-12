0285013208877a2d465f738d09c9b2acac4fa785
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-06-12 13:17:41 -0300

    Implementation of constants in PHP

---
+++ src/constants/constants.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>
+            Constants
+        </title>
+    </head>
+    <body>
+        <?php
+            echo("<h1>Constants</h1>");
+
+            /*
+                Constants in PHP
+
+                Exists two ways to create constants in PHP, using
+                the const keyword and using the declare function.
+
+                The difference between these two ways is the fact
+                about the impossibility to use the const keyword
+                to create constants inside another block scope.
+                If you want to do this, you'll need to use the
+                define function.
+
+                Last but not least, you have to keep in mind that
+                constants in PHP are global.
+            */
+
+            //  Creating a constant using the const keyword
+            const PERFECT_NUMBER = 42;
+            echo("<p>PERFECT_NUMBER: " . PERFECT_NUMBER . "</p>");
+
+            //  Creating a constant using the define function
+            define("PROCESSOR_CLOCK", 4.2);
+            echo("<p>PROCESSOR_CLOCK: " . PROCESSOR_CLOCK . "</p>");
+
+            //  Demonstrating the creation of constants inside a function
+            function create_const (): void   {
+                define("INSIDE_FUNCTION", (98375 / 2));
+            }
+
+            create_const();
+            
+            if (INSIDE_FUNCTION == (98375 / 2)) {
+                echo("<p>The constant INSIDE FUNCTION was created and its value is " . INSIDE_FUNCTION . ".</p>");
+            }   else    {
+                echo("<p>The constant INSIDE FUNCTION wasn't created.</p>");
+            }
+
+            //  Proving that constants in PHP are global
+            function print_some_constants (): void   {
+                echo("<p>PERFECT_NUMBER: " . PERFECT_NUMBER . "</p>");
+                echo("<p>PROCESSOR_CLOCK: " . PROCESSOR_CLOCK . "</p>");
+            }
+
+            print_some_constants();
+        ?>
+    </body>
+</html>
